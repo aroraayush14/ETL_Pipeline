0f41b97543a931e563ccd2475e97d3acb96c2930
aroraayush14 <aroraayush14@gmail.com>
2022-05-26 12:31:11 +0530

    Third Commit

--- etl.py
+++ etl.py
@@ -13,4 +13,9 @@ database = 'AdventureWorksDW2019'
 def extract():
     try:
         src_conn = pyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + '\SQLEXPRESS'   +  ';DATABASE=' + database + ';PGUID=' + uid + ';PWD=' + pwd)
-        
+        src_cursor = src_conn.cursor()
+        src_cursor.execute(""" select  t.name as table_name
+        from sys.tables t where t.name in ('DimProduct','DimProductSubcategory','DimProductSubcategory','DimProductCategory','DimSalesTerritory','FactInternetSales') """)
+        src_tables = src_cursor.fetchall()
+        for tbl in src_tables:
+            df = pd.read_sql_query
